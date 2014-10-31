39baa7000abea5a43ea58d8dd99971a2e7fcb5c4
Takashi Kokubun <takashikkbn@gmail.com>
2014-10-31 14:40:41 +0900

    Add run_tests.sh

---
+++ run_tests.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+testdirs=$(find . -type f -name "*\_test.go" | sed -e "s/[^\/]*$//" | uniq)
+
+for testdir in $testdirs; do
+  pushd $testdir > /dev/null
+  if result=$(go test); then
+    printf "\e[32m${result}\e[0m\n"
+  else
+    printf "\e[31m${result}\e[0m\n"
+  fi
+  popd > /dev/null
+done
