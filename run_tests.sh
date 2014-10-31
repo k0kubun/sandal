a154a20ee170d3983e577f9d352e408c973b413a
Takashi Kokubun <takashikkbn@gmail.com>
2014-10-31 17:26:02 +0900

    Fix test scripts

--- run_tests.sh
+++ run_tests.sh
@@ -11,3 +11,38 @@ for testdir in $testdirs; do
   fi
   popd > /dev/null
 done
+
+tempfile=`mktemp /tmp/tmp.XXXXXXXXXX`
+trap "rm $tempfile" 0
+
+for filename in `ls test`; do
+  case $filename in
+    (*.sandal)
+      actual_filename="test/${filename}"
+      expect_filename="test/${filename%.sandal}.expect"
+
+      if [ -e $expect_filename ]; then
+        err_output=`./sandal ${actual_filename} 2>&1 1>$tempfile`
+
+        if [ $? -ne 0 ]; then
+          printf "\e[31m"
+          echo "FAILED: ${actual_filename}"
+          echo "    ${err_output}"
+          printf "\e[0m"
+        else
+          diff_output=`diff -u $expect_filename $tempfile`
+
+          if [ $? -ne 0 ]; then
+            printf "\e[31m"
+            echo "FAILED: ${actual_filename}"
+            echo "${diff_output}" | awk '{ print "    " $0 }'
+            printf "\e[0m"
+          else
+            printf "\e[32m"
+            echo "PASSED: ${actual_filename}"
+            printf "\e[0m"
+          fi
+        fi
+      fi
+  esac
+done
